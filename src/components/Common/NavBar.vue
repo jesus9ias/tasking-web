03fd128a2bff5f9e8349708e8fd714ddec856985
Jesús Escamilla <jesusescamilla@hotmail.com>
2017-02-25 19:17:25 -0600

    basic navbar list

--- src/components/Common/NavBar.vue
+++ src/components/Common/NavBar.vue
@@ -1,6 +1,25 @@
 <template>
   <md-toolbar>
-    <h1 class="md-title">Tasking</h1>
+    <h1 class="md-title" style="flex: 1">Tasking</h1>
+
+    <md-menu md-size="4" md-direction="bottom left">
+      <md-button class="md-icon-button" md-menu-trigger>
+        <md-icon>face</md-icon>
+      </md-button>
+
+      <md-menu-content>
+        <md-menu-item>
+          <span>Settings</span>
+        </md-menu-item>
+
+        <md-menu-item>
+          <span>Log Out</span>
+        </md-menu-item>
+      </md-menu-content>
+    </md-menu>
+
+
+
   </md-toolbar>
 </template>
 
